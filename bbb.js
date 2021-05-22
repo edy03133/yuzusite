8d2527e39a525943c01194bb760eb5d2b71680b7
edy03133 <84275562+edy03133@users.noreply.github.com>
2021-05-22 09:15:11 +0900

    Add files via upload

--- bbb.js
+++ bbb.js
@@ -1,3 +1,21 @@
+// ここからふわっと表示
+window.sr = ScrollReveal({ reset: true, mobile: true });
+// 下から
+sr.reveal('.huwa', { origin: 'bottom', distance: '50px', duration: 600, scale: 1.0, delay: 400, });
+// 左から
+sr.reveal('.huwaLeft', { origin: 'left', distance: '200px', duration: 1000, scale: 1.0, delay: 400, });
+// 右から
+sr.reveal('.huwaRight', { origin: 'right', distance: '200px', duration: 1000, scale: 1.0, delay: 400, });
+// 右から
+sr.reveal('.huwaRight', { origin: 'right', distance: '30%', duration: 1000, scale: 1.0, delay: 400, });
+// delay2
+sr.reveal('.delay2', { origin: 'bottom', distance: '30%', duration: 1000, scale: 1.0, delay: 600, });
+// delay3
+sr.reveal('.delay3', { origin: 'bottom', distance: '30%', duration: 1000, scale: 1.0, delay: 1000, });
+
+// ここまでふわっと表示
+
+
 var bubbleLifeTime = 20;
 var noOfBubbles = 100;
 
@@ -77,3 +95,36 @@ $('#page-top').click(function () {
     return false;//リンク自体の無効化
 });
 // ここまでページトップ
+
+// ここからローディング(機能しないため、配置のみ)-----------------------
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});
+// ここまでローディング----------------------
